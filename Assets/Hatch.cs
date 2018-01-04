466222b527ea299537f0eeb1bff614ca51f53fe9
ETGVR <kristian.mansson@lucs.lu.se>
2018-01-04 16:12:54 +0100

    first attempt at hatch

---
+++ Assets/Hatch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hatch : MonoBehaviour {
+    private GameObject hatch;
+    private HingeJoint hinge;
+    private GameObject wheel;
+    private TextTracker textTracker;
+    // Use this for initialization
+    void Awake () {
+        hinge = GetComponent<HingeJoint>();
+
+        wheel = GameObject.Find("RotationalCylinder");
+        textTracker = wheel.GetComponent<TextTracker>();
+
+        JointSpring hingeSpring = hinge.spring;
+        hingeSpring.spring = 10;
+        hingeSpring.damper = 3;
+        hingeSpring.targetPosition = 20;
+        hinge.spring = hingeSpring;
+        hinge.useSpring = true;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        JointSpring hingeSpring = hinge.spring;
+        hingeSpring.targetPosition = textTracker.angle / 4;
+        hinge.spring = hingeSpring;
+        hinge.useSpring = true;
+    }
+}
--- Assets/TextTracker.cs
+++ Assets/TextTracker.cs
@@ -7,18 +7,26 @@ public class TextTracker : MonoBehaviour {
     private float startAngle;
     private Transform wheel;
     public Text textBox;
-
+    public float angle;
+    private Vector3 lastFwd;
+    private float curAngleX;
     private void Awake()
     {
         wheel = gameObject.transform;
         startAngle = wheel.rotation.eulerAngles.x;
     }
 
-    private float angle;
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
+        //angle = Mod(Mathf.CeilToInt(wheel.rotation.eulerAngles.x - startAngle), 360);
+        //textBox.text = "" + angle;
 
-        angle = Mod(Mathf.CeilToInt(wheel.rotation.eulerAngles.x - startAngle), 360);
-        textBox.text = "" + angle;
+        Vector3 curFwd = transform.forward;
+        //neg for correct orientation
+        float ang = -Vector3.SignedAngle(curFwd, lastFwd, transform.up);
+        angle += ang; 
+        lastFwd = curFwd; 
+        textBox.text = "" + Mathf.CeilToInt(angle);
     }
 
     int Mod(int a, int b)
