00dbefaf11b7b9a39b535d4757c24e241a8956f2
Alexander Cobleigh <tna11aco@student.lu.se>
2018-02-26 12:57:57 +0100

    Add beginnings of the HotkeyManager

---
+++ Assets/HotkeyManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HotkeyManager : MonoBehaviour {
+	
+	void Update () {
+        // restart the scene
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Restart the scene!");
+            SceneManager.LoadSceneAsync(0);
+        }
+		
+	}
+}
